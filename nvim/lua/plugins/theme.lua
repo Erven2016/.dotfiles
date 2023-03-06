aeb2284458ee0c31d3adf8630845f72d2e78cf90
Erven2016 <leiguihua2016@gmail.com>
2023-03-06 23:37:42 +0800

    feat(nvim-theme): use transparent background

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -5,7 +5,7 @@ return {
       theme_style = "dark_default",
       function_style = "italic",
       sidebars = { "qf", "vista_kind", "terminal", "packer" },
-      transparent = false,
+      transparent = true,
 
       -- Change the "hint" color to the "orange" color, and make the "error" color bright red
       -- colors = { hint = "orange", error = "#ff0000" },
